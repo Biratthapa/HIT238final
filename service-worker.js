206f96f239403e5685ff8026adeb7ee219529cf1
Biratthapa <48338951+Biratthapa@users.noreply.github.com>
2020-10-22 23:05:54 +0930

    fixed typos

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ var cacheFiles=[
   './style.css',
   './guitars.html',
   './laptop.html',
-  './smartphones.html'
+  './smartphones.html',
   './js/catalogue.js',
   './js/product.js',
   './js/test.js',
@@ -21,23 +21,30 @@ self.addEventListener('install', function(e) {
       return cache.addAll(cacheFiles);
     })
   )
-})
+});
 
 self.addEventListener('activate', function(e) {
   console.log('Service Worker:Activated');
   e.waitUntil(
 
-    caches.keys().then(function(cacheNames){
-       return Promise.all(cacheNames.map(function(thisCacheName){
-         if (thisCacheName !== cacheName) {
-           console.log("[ServiceWorker] Removing Cached Files from", thisCacheName);
-           return caches.delete(thisCacheName);
-         }
-       }))
+    caches.keys().then(function(names){
+       Promise.all(
+         names.filter(function(name){
+           return name !== cacheName;
+         }).map(function(name){
+           return caches.delete(name);
+         })
+
+       )
     })
   )
-})
+});
 
 self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.open(cacheName).then(function(cache){
+      return cache.match(e.request);
+    })
+  );
   console.log('Service Worker: fetching', e.request.url);
-})
+});
